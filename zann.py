da928373344f8733401660c2e9cbc8e2e2a9bf87
farzantugas <farzansekolah02@gmail.com>
2026-09-21 10:48:50 +0700

    Add files via upload

--- zann.py
+++ zann.py
@@ -1,13 +1,25 @@
-print("==================")
-print("=======zann=======")
-print("==================")
+print("================================")
+print("=========Sistem Nilai===========")
+print("================================")
+nama_siswa = input("Masukan nama siswa: ")
+nilai_ujian = int(input("Masukan nilai ujian (0-100): "))
 
-print('''perhatikannn goa inii
-
-|_| |_| |_| |_|
-
-]
-
-  ''')
-
-
+if nilai_ujian >= 90:
+    print("Nama siswa:", nama_siswa)
+    print("status: Lulus")
+    print("Predikat: A(Sangat Memuaskan)")
+elif nilai_ujian >= 80:
+     print("Nama siswa:", nama_siswa)
+     print("status: Lulus")
+     print("Predikat: B(Baik)")
+elif nilai_ujian >= 75:
+     print("Nama siswa:", nama_siswa)
+     print("status: Lulus")
+     print("Predikat: C(Cukup)")
+elif nilai_ujian < 75:
+     print("Nama siswa:", nama_siswa)
+     print("status: tidak")
+     print("Predikat: D(Tidak Lulus/Remedial)")
+else:
+     print("Pilihan tidak valid, Silahkan masukan nilai ujian antara 0-100")
+  
